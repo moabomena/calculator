82a39b1cfc1d13fdd42a0f3f482bbf95f641ee24
Moab.Omena <1moabomena@gmail.com>
2025-08-03 15:38:20 -0300

    add features:
    - addded a division button;
    
    - addded an ac button (all clean);
    
    - added calculus logic on operators;
    
    -and stylized the new buttons added;
    
    - created a history field in front of the results field.

---
+++ scripts/main.js
@@ -0,0 +1,134 @@
+let x = "0";
+let y = "0";
+let operator = "";
+let listX = [];
+let listY = [];
+let historic = "";
+let isAc = false;
+
+function setHistoric() {
+  historic = x;
+  historic += " " + operator + " ";
+  if (y != "0") {
+    historic += y;
+  }
+
+  showNumberHistoric(historic);
+}
+
+function updateList(list, number) {
+  if (list.length != 0) {
+    list.length = 0;
+    list = [...number];
+  }
+}
+
+function sum() {
+  return Number(x) + Number(y);
+}
+
+function subtraction() {
+  return Number(x) - Number(y);
+}
+
+function division() {
+  return Number(x) / Number(y);
+}
+
+function multiplication() {
+  return Number(x) * Number(y);
+}
+
+function getNumber(number) {
+  if (operator === "") {
+    if (x === "0") {
+      x = number;
+    } else {
+      x += number;
+    }
+    showNumberView(x);
+    updateList(listX, x);
+  } else {
+    if (y === "0") {
+      y = number;
+    } else {
+      y += number;
+    }
+    showNumberView(y);
+    updateList(listY, y);
+  }
+  setHistoric();
+}
+
+function showNumberView(number) {
+  const elementSum = document.getElementById("view-result");
+  elementSum.textContent = number;
+}
+
+function showNumberHistoric(number) {
+  const elementHistoric = document.getElementById("historic");
+  elementHistoric.textContent = number;
+}
+
+function getOperator(operatorValue) {
+  operator = operatorValue;
+  setHistoric();
+}
+
+function clean() {
+  x = "0";
+  y = "0";
+  operator = "";
+  showNumberView("0");
+  showNumberHistoric(" ");
+}
+
+function result() {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = sum();
+      break;
+    case "-":
+      result = subtraction();
+      break;
+    case "/":
+      result = division();
+      break;
+    case "x":
+      result = multiplication();
+      break;
+    default:
+      break;
+  }
+  setHistoric();
+  const elementSum = document.getElementById("view-result");
+  elementSum.textContent = result;
+}
+
+function backNumber(list, number) {
+  if (list.length === 0) {
+    if (number != "0") {
+      list = [...number];
+    }
+  }
+
+  if (list.length != 0) {
+    list.pop();
+    number = list.join("");
+    const updateNumber = document.getElementById("view-result");
+    updateNumber.textContent = number;
+  }
+  return list;
+}
+
+function updateNumbers() {
+  x = listX.join("");
+  y = listY.join("");
+}
+
+function backspace() {
+  listX = backNumber(listX, x);
+  listY = backNumber(listY, y);
+  updateNumbers();
+}
